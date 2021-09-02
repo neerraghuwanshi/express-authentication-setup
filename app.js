587dfb189ef5c197be534d3481f578f8460ec910
Neer Raghuwanshi <raghuwanshineer1@gmail.com>
2021-09-03 01:14:58 +0530

    Built only 3 routes for /user - login, signup and initials

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+app.use(helmet());
+app.use(compression());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        '*'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Accept, Origin, Content-Type, Authorization'
+    );
+    next();
+});
+  
+app.use('/user', userRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Page not Found')
+    error.statusCode = 404
+    next(error)
+});
+
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    res.status(status).json({ 
+        message: error.message, 
+        data: error.data,
+    });
+});
+
+const DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.gvbrv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  
+mongoose.connect(DB_URI)
+.then(result => {
+    app.listen(8000);
+})
+.catch(err => console.log(err));
