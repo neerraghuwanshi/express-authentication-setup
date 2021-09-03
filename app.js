6549ad21ed430bfc7dd784d717452be934a632e7
Neer Raghuwanshi <raghuwanshineer1@gmail.com>
2021-09-03 15:54:22 +0530

    Added cors package to set cors headers and removed code that manually sets headers.

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const helmet = require('helmet');
 const compression = require('compression');
 
 const userRoutes = require('./routes/user');
 
+
 const app = express();
 
+app.use(cors());
 app.use(helmet());
 app.use(compression());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        '*'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Accept, Origin, Content-Type, Authorization'
-    );
-    next();
-});
   
 app.use('/user', userRoutes);
 
